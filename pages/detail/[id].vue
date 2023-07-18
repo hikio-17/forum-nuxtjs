487001d450ef7c1f7acb815f07e090537d75b8c2
hikio-17 <fajri010217@gmail.com>
2023-07-18 02:49:18 -0700

    change file

--- pages/detail/[id].vue
+++ pages/detail/[id].vue
@@ -1,5 +1,5 @@
 <script setup>
-   import { onMounted, computed } from 'vue';
+   import { onMounted } from 'vue';
    import { storeToRefs } from 'pinia';
    import { useTalkDetail } from '@/store/talkDetail';
    import { useAuthUser } from '@/store/authUser';
